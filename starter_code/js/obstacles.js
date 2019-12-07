ba366aef61695001ec630fc236eaf2c1f7102bae
Miguel <alonsogalalrdomiguel@gmail.com>
2019-12-07 12:57:45 +0100

    creating obstacles v2

---
+++ starter_code/js/obstacles.js
@@ -0,0 +1,13 @@
+class Obstacles{
+    constructor(){
+        this.image = new Image();
+        this.image.src ="images/obstacle_top.png";
+        this.image = new Image1();
+        this.image.src ="images/obstacle_bottom.png";
+    }
+    draw(){
+
+
+    }
+    move()
+}
