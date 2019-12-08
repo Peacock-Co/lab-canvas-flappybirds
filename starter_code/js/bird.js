d048b14ff085dfe9816fda7363469b28fdc227f5
Carlos Mateo <cmc2k5@gmail.com>
2019-12-08 16:31:19 +0100

    Adds Faby sprites

--- starter_code/js/bird.js
+++ starter_code/js/bird.js
@@ -1,31 +1,63 @@
-class Bird{
-    constructor(ctx){
+class Bird {
+    constructor(ctx, canvasHeight) {
         this.ctx = ctx;
-        this.width = 70;
-        this.height = 60;
-        this.image = new Image();
-        this.image.src = "images/flappy.png"
+        this.canvasHeight = canvasHeight;
+
+        this.width = 68;
+        this.height = 48;
         this.x = 200;
-        this.y = 250 - this.height / 2 ;
-        this.birdFall = 0
-        this.setListener()
+        this.y = this.canvasHeight / 2 - this.height / 2;
 
+        this.image = new Image();
+        this.image.src = "images/flappy_sprites.png";
 
+        this.frames = 3;
+        this.framesIndex = 0;
+
+        this.gravity = 4;
+        this.hasGameStarted = false;
+
+        this.setListener();
     }
 
-    draw(){
-       this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    draw(frameCounter) {
+        this.ctx.drawImage(
+            this.image,
+            this.framesIndex * Math.floor(this.image.width / this.frames),
+            0,
+            Math.floor(this.image.width / this.frames),
+            this.image.height,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+
+        this.animateFlappy(frameCounter);
     }
 
-    move(){
-        
+    move() {
+        if (this.hasGameStarted) {
+            this.y += this.gravity;
+            this.gravity += 0.4;
+        }
     }
 
-    setListener(){
-        document.addEventListener("keydown",function(e){
-            if(e.keyCode == 32){
-              this.y -=7
+    animateFlappy(frameCounter) {
+        if (frameCounter % 10 === 0) {
+            this.framesIndex++;
+
+            this.framesIndex = this.framesIndex > 2 ? 0 : this.framesIndex;
+        }
+    }
+
+    setListener() {
+        document.addEventListener("keydown", function (e) {
+            if (e.keyCode === 32) {
+                if (!this.hasGameStarted) this.hasGameStarted = true;
+                this.y -= this.gravity;
+                this.gravity -= 10;
             }
-          })
+        }.bind(this));
     }
 }
